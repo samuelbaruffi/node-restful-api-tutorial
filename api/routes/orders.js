11df0415350f47de042e9606c9a5a3a7a986e73e
sbaruffi <sbaruffi@amazon.com>
2020-02-14 17:08:21 -0800

    First commit

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,36 +1,65 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+
+const Order = require("../models/order");
 
 // Handle incoming GET requests to /orders
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Orders were fetched'
+router.get("/", (req, res, next) => {
+  Order.find()
+    .select("product quantity _id")
+    .exec()
+    .then(docs => {
+      res.status(200).json({
+        count: docs.length,
+        oreders: docs.map(doc => {
+          return {
+            _id: doc._id,
+            product: doc.product,
+            quantity: doc.quantity,
+            request: {
+              type: "GET",
+              url: "http://localhost:3000/orders/" + doc._id
+            }
+          };
+        })
+      });
+    })
+    .catch(err => {
+      res.status(500).json(err);
     });
 });
 
-router.post('/', (req, res, next) => {
-    const order = {
-        productId: req.body.productId,
-        quantity: req.body.quantity
-    };
-    res.status(201).json({
-        message: 'Order was created',
-        order: order
+router.post("/", (req, res, next) => {
+  const order = new Order({
+    _id: mongoose.Types.ObjectId(),
+    quantity: req.body.quantity,
+    product: req.body.productId
+  });
+  order
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
     });
 });
 
-router.get('/:orderId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Order details',
-        orderId: req.params.orderId
-    });
+router.get("/:orderId", (req, res, next) => {
+  res.status(200).json({
+    message: "Order details",
+    orderId: req.params.orderId
+  });
 });
 
-router.delete('/:orderId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Order deleted',
-        orderId: req.params.orderId
-    });
+router.delete("/:orderId", (req, res, next) => {
+  res.status(200).json({
+    message: "Order deleted",
+    orderId: req.params.orderId
+  });
 });
 
 module.exports = router;
--- api/routes/products.js
+++ api/routes/products.js
@@ -7,11 +7,25 @@ const Product = require("../models/product");
 router.get("/", (req, res, next) => {
   console.log("it got here");
   Product.find()
+    .select("name price _id")
     .exec()
     .then(docs => {
-      console.log(docs);
+      const response = {
+        count: docs.length,
+        products: docs.map(doc => {
+          return {
+            name: doc.name,
+            price: doc.price,
+            _id: doc._id,
+            request: {
+              type: "GET",
+              url: "http://localhost:3000/products/" + doc._id
+            }
+          };
+        })
+      };
       //   if (docs.length >= 0) {
-      res.status(200).json(docs);
+      res.status(200).json(response);
       //   } else {
       //       res.status(404).json({
       //           message: 'No entries found'
@@ -40,8 +54,16 @@ router.post("/", (req, res, next) => {
     .then(result => {
       console.log(result);
       res.status(201).json({
-        message: "Handling POST requests to /products",
-        createdProduct: result
+        message: "Created product successfully",
+        createdProduct: {
+          name: result.name,
+          price: result.price,
+          _id: result._id,
+          request: {
+            type: "GET",
+            url: "http://localhost:3000/products/" + result._id
+          }
+        }
       });
     })
     .catch(err => {
@@ -55,11 +77,18 @@ router.post("/", (req, res, next) => {
 router.get("/:productId", (req, res, next) => {
   const id = req.params.productId;
   Product.findById(id)
+    .select("name price _id")
     .exec()
     .then(doc => {
       console.log("From database", doc);
       if (doc) {
-        res.status(200).json(doc);
+        res.status(200).json({
+          product: doc,
+          request: {
+            type: "GET",
+            url: "http://localhost/products"
+          }
+        });
       } else {
         res
           .status(404)
@@ -78,11 +107,16 @@ router.patch("/:productId", (req, res, next) => {
   for (const ops of req.body) {
     updateOps[ops.propName] = ops.value;
   }
-  Product.update({ _id: id }, { $set: updateOps })
+  Product.updateOne({ _id: id }, { $set: updateOps })
     .exec()
     .then(result => {
-      console.log(result);
-      res.status(200).json(result);
+      res.status(200).json({
+        message: "Product Updated",
+        request: {
+          type: "GET",
+          url: "http://localhost:3000/products/" + id
+        }
+      });
     })
     .catch(err => {
       console.log(err);
@@ -97,7 +131,14 @@ router.delete("/:productId", (req, res, next) => {
   Product.remove({ _id: id })
     .exec()
     .then(result => {
-      res.status(200).json(result);
+      res.status(200).json({
+        message: "Product deleted",
+        request: {
+          type: "POST",
+          url: "http://localhost:3000/products",
+          body: { name: "String", price: "Number" }
+        }
+      });
     })
     .catch(err => {
       console.log(err);
